0006cd87fe887757263febfb38be966c1d541684
rniemeyer07 <rniemeyr@uw.edu>
2016-03-31 14:44:44 -0700

    updated reservoir loop and some of the reservoir info

--- tools/reservoir_info_to_network.py
+++ tools/reservoir_info_to_network.py
@@ -2,35 +2,58 @@
 
 # ------------------------------------------------------------------------------------
 #
-#           Code to read in _Network file and add reservoir information
+#           Code to read in _Network file and add reservoir information 
 #
 # ------------------------------------------------------------------------------------
 
 import sys
+import csv
+import pandas as pd
+import os.path
 
 # ------------------------------------------------------------------------------------
 #        read in reservoir information, write all nodes with a reservoir
 # ------------------------------------------------------------------------------------
 
-f = open(sys.argv[1])  # run the script like this: ./reservoir_info_to_network.py reservoir_to_model.csv
-csv_f = csv.reader(f)
+# reservoir_file = 'reservoir_to_model.csv'
+reservoir_file = sys.argv[2]
+with open(reservoir_file, 'rU') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',', dialect=csv.excel_tab)
+	headers = csvfile.next() # read in headers
+	#headers = headers[0]
+	headers = headers.split(',')
+	headers[len(headers)-1] = headers[len(headers)-1].rstrip('\n')  #strip \n
+	reservoir_nodes = []
+	for row in reader:
+		# print ', '.join(row)
+		x = ', '.join(row)
+		x = x.split(',')
+		reservoir_nodes.append(x)
 
-# ----- loop to read in first and last nodes, sequence across those, and get
-#        a long array with all the nodes with reservoirs
-reservoir_nodes = []
-for row in csv_f:
-        x = row[3:5]  # get the first and last node
-        x = range(int(x[0]),(int(x[1])+1))
-        reservoir_nodes.extend(x)
+reservoir_nodes = pd.DataFrame(reservoir_nodes)	
+reservoir_nodes.columns = headers
+
+# -------- sequence of numbers -----------
+reservoir_tot_nodes = []
+reservoir_tot_nodes_index = []
+for i in range(0, len(reservoir_nodes['start_node']) ):
+	x = range(int(reservoir_nodes['start_node'][i]), int(reservoir_nodes['end_node'][i])+1 )
+	reservoir_tot_nodes.extend(x)
+	x = repeat(i+1, len(x) )  # want index to start with 1, not 0
+	reservoir_tot_nodes_index.extend(x)
+
+reservoir_tot_nodes2 = pd.DataFrame(reservoir_tot_nodes)
 
 # ------------------------------------------------------------------------------------
 #        read in reservoir information, write all nodes with a reservoir
 # ------------------------------------------------------------------------------------
 
-with open("Salmon_0.5_Network") as f:
-    network =  f.read().splitlines()
+# network_file = "Holston_8th_Network"
+network_file = sys.argv[1]
+with open(network_file) as f:
+    network =  f.read().splitlines() 
 
-# -------- get dimensions of river network ----------
+# -------- get dimensions of river network ----------    
 str = network[4]
 dim = [int(s) for s in str.split() if s.isdigit()]  # nreach, flow_cells, heat_cells, source
 
@@ -38,29 +61,37 @@ dim = [int(s) for s in str.split() if s.isdigit()]  # nreach, flow_cells, heat_c
 network2 = network[0:4]
 x = network[4] + '     TRUE'  # a boolean for if reservoirs are simulated with two-layer models
 network2.append(x)
-
+				
 # ------------- loop to get the nodes each file --------
 for i in range(5, len(network)-1):
-        x = network[i]
-        if x[0:4] == 'Node':
-                x2 = [int(s) for s in x.split() if s.isdigit()]
-
-                # if node in NETWORK file has a reservoir
-                if x2[0] in reservoir_nodes:
-                        x3 = network[i] + '     TRUE'
-                        network2.append(x3)
-
-                # if node in NETWORK file does not have reservoir
-                else:
-                        x3 = network[i] + '     FALSE'
-                        network2.append(x3)
-        else:
-                network2.append(x)
+	x = network[i]
+	if x[0:4] == 'Node':
+		x2 = [int(s) for s in x.split() if s.isdigit()]
+		
+		# if node in NETWORK file has a reservoir
+		if x2[0] in reservoir_tot_nodes:
+			
+			nodesx = int(np.where(reservoir_tot_nodes2 == x2[0])[0])
+			nodesx2 = reservoir_tot_nodes_index[nodesx]
+			x4 = nodesx2.astype('|S10')
+			x4 = x4.rjust(5-len(x4))
+			x3 = network[i] + '  ' +  x4 + '  TRUE '
+			network2.append(x3)
+			
+		# if node in NETWORK file does not have reservoir
+		else:
+			x3 = network[i] + '  ' +  '   0'  + '  FALSE'
+			network2.append(x3)
+	else:
+		network2.append(x)
 
 # ------------- write the file --------
-with open ('myfile', 'w') as fo:
+network_file = network_file + "_2"  # REMOVE once you have a network file you like (i.e. overwrite network file)
+save_path = '/raid3/rniemeyr/RBM/two_layer_model/RBM_Yixin/RIPS/model_run/source/VIC_RBM/RBM_processing/Holston_8th.Mohseni_v1/'
+completeName = os.path.join(save_path, network_file)         
+
+with open (network_file, 'w') as fo:
    for d in network2:
      fo.write(d + '\n')
-
-
-
+     
+    
