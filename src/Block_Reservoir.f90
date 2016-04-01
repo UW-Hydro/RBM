e84dbf9e6f3b353851f6bf09e4d3c5a2bd46b13a
Yifan Cheng <yifan@hydra.hydro.washington.edu>
2016-04-01 12:17:36 -0700

    add Block_Reservoir.f90

---
+++ src/Block_Reservoir.f90
@@ -0,0 +1,13 @@
+module Block_Reservoir
+
+        implicit none
+
+        !------------------------read in reservoir info--------------------------
+
+        real, dimension (:), allocatable:: dam_lat, dam_lon, res_grid_lat, res_grid_lon
+        real, dimension (:), allocatable:: res_depth_feet, res_width_feet, res_length_feet
+        integer, dimension (:), allocatable:: dam_number, start_operating_year
+        integer, dimension (:), allocatable:: res_top_vol, res_bot_vol, res_max_flow, res_min_flow
+        integer, dimension (:), allocatable:: res_start_node, res_end_node
+
+end module Block_Reservoir
