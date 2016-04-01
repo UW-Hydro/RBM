eac49213e2bce15e8c95cdd0302b258727c8caff
rniemeyer07 <rniemeyr@uw.edu>
2016-04-01 10:25:38 -0700

    indented Particle_Tracking.f90 and added some comments

--- src/Particle_Track.f90
+++ src/Particle_Track.f90
@@ -11,63 +11,72 @@ real:: dt_total
 !     First do the reverse particle tracking
 !
 DO ns=no_celm(nr),1,-1  ! loops from no_celm(nr) to 1, backwards by 1
-!
-!     Segment is in cell SEGMENT_CELL(NC)
-!
-   ncell=segment_cell(nr,ns)
-   print *, nr, ns, ncell  
-   nx_s=1
-   nx_part=ns
-   dt_part(ns)=dt(ncell)
-   dt_total=dt_part(ns)
-   x_part(ns)=x_dist(nr,ns)
+    !
+    !     Segment is in cell SEGMENT_CELL(NC)
+    !
+    ncell=segment_cell(nr,ns) !cell and/or node # for specific reach and segment 
+    print *, nr, ns, ncell  
+    nx_s=1
+    nx_part=ns                   ! index number for segment?
+    dt_part(ns)=dt(ncell)        ! change in time?
+    dt_total=dt_part(ns)         ! total elapsed travel time
+    x_part(ns)=x_dist(nr,ns)     ! river mile distance of that segment
 
-100 CONTINUE
+    100 CONTINUE
 
-!
-!     Determine if the total elapsed travel time is equal to the
-!     computational interval
-!
-   IF(dt_total.lt.dt_comp) THEN
-      x_part(ns)=x_part(ns)+dx(segment_cell(nr,nx_part))
-!
-!     If the particle has started upstream from the boundary point, give it
-!     the value of the boundary
-!
-      IF(x_part(ns).GE.x_bndry) THEN
-         x_part(ns)=x_head
-         dt_part(ns)=dt(segment_cell(nr,nx_part))
-         dt_total=dt_total+dt_part(ns)
-         GO TO 200
-      END IF
-!
-!     Increment the segment counter if the total time is less than the
-!     computational interval
-!
+    !
+    !     Determine if the total elapsed travel time is equal to the
+    !     computational interval
+    !
+    IF(dt_total.lt.dt_comp) THEN ! if total time is less than the # of seconds in time step
+       x_part(ns)=x_part(ns) + dx(segment_cell(nr,nx_part))   
+    !
+    !     If the particle has started upstream from the boundary point, give it
+    !     the value of the boundary
+    !
+        IF(x_part(ns).GE.x_bndry) THEN
+          x_part(ns)=x_head
+          dt_part(ns)=dt(segment_cell(nr,nx_part))
+          dt_total=dt_total+dt_part(ns)
 
-      nx_s=nx_s+1
-      nx_part=nx_part-1
-      dt_part(ns)=dt(segment_cell(nr,nx_part))
-      dt_total=dt_total+dt_part(ns)
-      GO TO 100
-   ELSE
-!
-!     For the last segment of particle travel, adjust the particle location
-!     such that the total particle travel time is equal to the computational
-!     interval.
-!
-      dt_part(ns)=dt_comp-dt_total+dt_part(ns)
-      x_part(ns)=x_part(ns)+u(segment_cell(nr,nx_part))*dt_part(ns)
-      IF(x_part(ns).GE.x_head) THEN
-         x_part(ns)=x_head
-         nx_s=nx_s-1
-         dt_part(ns)=dt(head_cell(nr))
-      END IF
-   END IF
-200 CONTINUE
-   IF(nx_part.LT.1) nx_part=1
-   nstrt_elm(ns)=nx_part
-   no_dt(ns)=nx_s
+          GO TO 200
+
+        END IF
+    !
+    !     Increment the segment counter if the total time is less than the
+    !     computational interval
+    !
+
+        nx_s=nx_s+1
+        nx_part=nx_part-1
+        dt_part(ns)=dt(segment_cell(nr,nx_part))
+        dt_total=dt_total+dt_part(ns)
+
+        GO TO 100
+
+    ELSE    ! if the total travel time is the same as the time step (computational interval)
+        !
+        !     For the last segment of particle travel, adjust the particle location
+        !     such that the total particle travel time is equal to the computational
+        !     interval.
+        !
+        dt_part(ns)=dt_comp-dt_total+dt_part(ns)
+        x_part(ns)=x_part(ns)+u(segment_cell(nr,nx_part))*dt_part(ns)
+
+        IF(x_part(ns).GE.x_head) THEN
+            x_part(ns)=x_head
+            nx_s=nx_s-1
+            dt_part(ns)=dt(head_cell(nr))
+        END IF
+
+    END IF
+
+    200 CONTINUE
+
+    IF(nx_part.LT.1) nx_part=1
+    nstrt_elm(ns)=nx_part
+    no_dt(ns)=nx_s
+
 END DO
+
 END SUBROUTINE Particle_Track
-!END MODULE P_Track
