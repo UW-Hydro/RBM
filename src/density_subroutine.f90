5a94fe313a62c83dff8f4704c5cb1cc70fbfb9bc
John Yearsley <jyearsle@uw.edu>
2016-05-14 10:29:49 -0700

    Comments are submitted as: [text - JRY].
    This subroutine (or it could be a function) should be called with the appropriate temperature as an argument

--- src/density_subroutine.f90
+++ src/density_subroutine.f90
@@ -1,38 +1,15 @@
-subroutine stream_density(nresx)
-! subroutine stream_density(T_epil, T_hypo, stream_T_in, density_epil,density_hypo, density_in, nresx)
-! subroutine stream_density(T_epil(nresx), T_hypo(nresx), stream_T_in(nresx), density_epil(nresx),density_hypo(nresx), density_in(nresx), nresx)
- use Block_Reservoir
-
+subroutine stream_density(SOME_TEMPERATURE,THE_DENSITY)
+[NO BLANK LINES - JRY]
  implicit none
 
- ! real, dimension (:), allocatable :: T_epil,T_hypo, volume_e_x, volume_h_x
- ! real, dimension (:), allocatable :: stream_T_in
- ! real, dimension (:), allocatable :: density_epil, density_hypo, density_in
-  integer :: nresx
+ ! real :: SOME_TEMPERATURE, THE_DENSITY
 
-!  allocate (T_epil(nres))
-! allocate (T_hypo(nres))
-! allocate (stream_T_in(nres))
-! allocate (density_epil(nres))
-! allocate (density_hypo(nres))
-! allocate (density_in(nres))
 
 !-------------------calculate the density based on temperature----------------
-    density_epil(nresx) = 1.000028*1e-3*((999.83952+16.945176*T_epil(nresx))-& 
-        & (7.9870401e-3*(T_epil(nresx)**2)-46.170461e-6*(T_epil(nresx)**3))+&
-        & (105.56302e-9*(T_epil(nresx)**4)-280.54235e-12*(T_epil(nresx)**5)))/&
-        & (1+16.87985e-3*T_epil(nresx))
-    density_hypo(nresx) = 1.000028*1e-3*((999.83952+16.945176*T_hypo(nresx))-&
-        & (7.9870401e-3*(T_hypo(nresx)**2)-46.170461e-6*(T_hypo(nresx)**3))+ &
-        & (105.56302e-9*(T_hypo(nresx)**4)-280.54235e-12*(T_hypo(nresx)**5)))/&
-        & (1+16.87985e-3*T_hypo(nresx))
-    density_in(nresx) = 1.000028*1e-3*((999.83952+16.945176*T_res_in(nresx))&
-        & -(7.9870401e-3*(T_res_in(nresx)**2)-46.170461e-6*&
-        & (T_res_in(nresx)**3))+(105.56302e-9*(T_res_in(nresx)**4)- &
-        & 280.54235e-12*(T_res_in(nresx)**5)))/(1+16.87985e-3* &
-        & T_res_in(nresx))
-
-! print *, 'T_epil', T_epil(nresx), 'T_hypo', T_hypo(nresx), 'T_res_in', T_res_in(nresx)
+    THE_DENSITY = 1.000028*1e-3*((999.83952+16.945176*SOME_TEMPERATURE-& 
+        & (7.9870401e-3*(SOME_TEMPERATURE**2)-46.170461e-6*(SOME_TEMPERATURE**3))+&
+        & (105.56302e-9*(SOME_TEMPERATURE**4)-280.54235e-12*(SOME_TEMPERATURE**5)))/&
+        & (1+16.87985e-3*SOME_TEMPERATURE)
 
 end subroutine stream_density
 
