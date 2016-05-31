0e4b85c332b7db18e63200622077344b3029a27e
Yifan Cheng <yifan@hydra.hydro.washington.edu>
2016-05-31 15:14:59 -0700

    change res_top_vol to float in Block_reservoir.f90

--- src/Block_Reservoir.f90
+++ src/Block_Reservoir.f90
@@ -7,10 +7,10 @@ module Block_Reservoir
         !----------------------------------------------------------------------
 
         !------------------------read in reservoir info--------------------------
-        real, dimension (:), allocatable:: dam_lat, dam_lon, res_grid_lat, res_grid_lon
+        real, dimension (:), allocatable:: dam_lat, dam_lon, res_grid_lat, res_grid_lon, res_top_vol
         real, dimension (:), allocatable:: res_depth_feet, res_width_feet, res_length_feet
         integer, dimension (:), allocatable:: dam_number, start_operating_year
-        integer, dimension (:), allocatable:: res_top_vol, res_bot_vol, res_max_flow, res_min_flow
+        integer, dimension (:), allocatable:: res_bot_vol, res_max_flow, res_min_flow
         integer, dimension (:), allocatable:: res_start_node, res_end_node
        !  integer, dimension (:,:), allocatable:: nodes_x   !for each reach, what are all the nodes
         real , dimension(:), allocatable  :: rmile_node
