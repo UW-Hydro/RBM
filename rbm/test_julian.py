51a09a1ae7ac43897f2d38cc034f9b5e7f0e395d
ChengYifan <chengyifan@D-108-179-181-180.dhcp4.washington.edu>
2016-10-25 14:16:16 -0700

    update .travis file

--- rbm/test_julian.py
+++ rbm/test_julian.py
@@ -1,10 +1,16 @@
 import leapyear
+import pytest
+import datetime
+import calendar
 import numpy as np
+import pandas as pd
 
-test_date = [1900, 2000, 2001, 2004]
-answer = [0,1,0,1]
-
-for i in range(len(test_date)):
-    determine = leapyear.leapyear(test_date[i]) == answer[i]
-    if determine != True:
-        print('Wrong!')
+def test_leap_year():
+    for year in np.arange(1900, 2100):
+        actual = calendar.isleap(year)
+        if actual == False:
+            actual = 0
+        else:
+            actual = 1
+        assert not bool(vic_lib.leap_year(year, calendars['noleap']))
+        assert actual == leapyear.leapyear(year)
