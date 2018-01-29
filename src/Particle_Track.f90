8e22071b3379ab4b56361314e34fbfb7b12bb4fb
Y. Cheng <yifanc6@ikt1.hyak.local>
2018-01-29 15:18:30 -0800

    fix minor error in particle tracking branch

--- src/Particle_Track.f90
+++ src/Particle_Track.f90
@@ -29,12 +29,13 @@ SUBROUTINE Particle_Track(nr,ns,nx_s,nx_head,ns_res_pres,ns_res_num)
         ncell=segment_cell(nr,nx_part)
         dt_part(nx_s)=dt(ncell)
         dt_total=dt_total+dt_part(nx_s)
+        if (dt_total .le. dt_comp) nx_part_next=nx_part_next-1
         !
         !     Increment the segment counter if the total time is less than the
         !     computational interval
         !
         if (nx_s.gt.1) nx_part=nx_part-1
-        nx_part_next=nx_part_next-1
+        !nx_part_next=nx_part_next-1
         if (nx_part_next .gt. 0) then
             ns_res_pres = res_pres(segment_cell(nr,nx_part_next))
         end if 
